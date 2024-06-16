1b81587436d0d4966a1ba4c416978fa684c4fddd
CJ Bunyi <cjb.nyi@gmail.com>
2024-06-17 00:52:16 +0800

    Finished infix-to-postfix.h

--- src/infix-to-postfix.h
+++ src/infix-to-postfix.h
@@ -19,31 +19,82 @@
  * @param Output
  * @retval 0 if returns a number
  * @retval 1 if returns a string
+ * @retval 2 if end of string
  */
 int parseStringInput(char *Input, int *nthInputChar, int *nOutputNumber, char *nOutputOperation) {
   String63 token = "";
-  int isNotWhiteSpace = Input[*nthInputChar] != ' ' && Input[*nthInputChar] != '\0'; // cj - we don't need this anymore since we won't have blank spaces anw
-  int i;
+  char currChar = Input[*nthInputChar];
+  int isCurrCharNull = currChar == '\0';
+  int isCurrCharNumber = currChar >= '0' && currChar <= '9';
 
   LOG(LPOST, "Processing TOKEN:\n");
-  // Parses the token
-  while (isNotWhiteSpace) {
-    strncat(token, Input + *(nthInputChar), 1); // safer to use strncat(), and its useful to select certain characters.
-    (*nthInputChar)++;
-    isNotWhiteSpace = Input[*nthInputChar] != ' ' && Input[*nthInputChar] != '\0';
-    LOG(LPOST, "\t%s\n",token);
-  }
-  LOG(LPOST, "TOKEN := %s\n", token);
+  // End of string has been reached
+  if (isCurrCharNull)
+    return 2;
 
-  // Ignore the white space
-  while (Input[*nthInputChar] == ' ') // cj - we can remove this too
-    (*nthInputChar)++;
+  // Parses the token
+  if (isCurrCharNumber) { // parsing a operand
+    while (!isCurrCharNull && isCurrCharNumber) {
+      strncat(token, Input + *(nthInputChar), 1); // safer to use strncat(), and its useful to select certain characters.
+      (*nthInputChar)++;
+
+      currChar = Input[*nthInputChar];
+      isCurrCharNull = currChar != '\0';
+      isCurrCharNumber = currChar >= '0' && currChar <= '9';
+
+      LOG(LPOST, "\t%s\n", token);
+    }
+    LOG(LPOST, "TOKEN := %s\n", token);
+  }
+  else { // parsing an operator
+    if (currChar == '>' || currChar == '<' || currChar == '!') {
+      int nIsEquals = 0; // next char is '='
+      int nnIsEquals = 0; // next next char is '='
+      int nnnIsEquals = 0; // next next next char is '='
+
+      if (Input[*nthInputChar + 1] == '=') {
+        nIsEquals = 1;
+        if (Input[*nthInputChar + 2] == '=') {
+          nnIsEquals = 1;
+          if (Input[*nthInputChar + 3] == '=') {
+            nnnIsEquals = 1;
+          }
+        }
+      }
+
+      // Let ? be < or >
+      if (!nIsEquals) { // ?
+        strncat(token, Input + *(nthInputChar), 1);
+        (*nthInputChar)++;
+      }
+      else if (!nnIsEquals) { // ?=
+        strncat(token, Input + *(nthInputChar), 2);
+        (*nthInputChar) += 2;
+      }
+      else if (!nnnIsEquals) { // ? followed by ==
+        strncat(token, Input + *(nthInputChar), 1);
+        (*nthInputChar)++;
+      }
+      else { // ?= followed by ==
+        strncat(token, Input + *(nthInputChar), 2);
+        (*nthInputChar) += 2;
+      }
+    }
+    else if (currChar == '=' || currChar == '&' || currChar == '|') {
+      strncat(token, Input + *(nthInputChar), 2);
+      (*nthInputChar) += 2;
+    }
+    else { // 1-char operators
+      strncat(token, Input + *(nthInputChar), 1);
+      (*nthInputChar)++;
+    }
+  }
 
   *nOutputNumber = atoi(token);
   if (*nOutputNumber > 0)
-    return 0 ;
+    return 0;
   
-  // if not a number by atoi, check if its char '0';
+  // if not a number by atoi, check if it's char '0';
   else if (strlen(token) == 1 && token[0] == '0') {
     *nOutputNumber = 0;
     return 0;
@@ -62,8 +113,10 @@ int parseStringInput(char *Input, int *nthInputChar, int *nOutputNumber, char *n
  */
 int infixToPostfix(String255 infixString) {
     Stack* OperatorStack = createStack();
-    queue* PostfixQueue = createQueue();
-    
+    queue* PostfixQueue  = createQueue();
+    struct Operation OperationTable[MAX_NUM_OPERATIONS];
+    initOperatorTable(OperationTable);
+
     String7 stringOperation;
     int nthInfixChar    = 0;
     int nextParseState  = 0;
@@ -75,10 +128,13 @@ int infixToPostfix(String255 infixString) {
 
     int currNumber;
     String7 currNumberString;
-    String7 currOperation;
+
+    String7 currOperationString, topOperationString;
+    int currOperationIndex, topOperationIndex;
+    struct Operation currOperation, topOperation;
 
     while (isConvertingInfix) {
-      nextParseState = parseStringInput(infixString, &nthInfixChar, &currNumber, &currOperation);
+      nextParseState = parseStringInput(infixString, &nthInfixChar, &currNumber, currOperationString);
 
       switch(nextParseState) {
         case 0:
@@ -87,14 +143,50 @@ int infixToPostfix(String255 infixString) {
           enqueue(PostfixQueue, currNumberString);
           break;
         case 1:
-          LOG(LPOST, "TYPE: Operator (%s)\n\n", currOperation);
-          // TODO: finish this part
+          LOG(LPOST, "TYPE: Operator (%s)\n\n", currOperationString);
+          
+          // TODO: finish this
+          currOperationIndex = searchOperatorTable(OperationTable, currOperationString);
+          currOperation = OperationTable[currOperationIndex];
+
+          if (strcmp(currOperationString, "(") == 0) { // just enqueue "("
+            enqueue(PostfixQueue, currOperationString);
+          }
+          else if (strcmp(currOperationString, ")") == 0) { // pop until first "("
+            while (strcmp(stackTop(OperatorStack), "(") != 0) {
+              enqueue(PostfixQueue, pop(OperatorStack));
+            }
+          }
+          else { // pop until emptied stack or first operator with lower precedence
+            int isFinishedPopping = 0;
+
+            while (!isStackEmpty(OperatorStack) && !isFinishedPopping) {
+              strcpy(topOperationString, stackTop(OperatorStack));
+              topOperationIndex = searchOperatorTable(OperationTable, topOperationString);
+              topOperation = OperationTable[topOperationIndex];
+
+              if (topOperation.nPrecedence >= currOperation.nPrecedence) {
+                enqueue(PostfixQueue, pop(OperatorStack));
+              }
+              else {
+                isFinishedPopping = 1;
+              }
+            }
+
+            enqueue(PostfixQueue, currOperationString);
+          }
+          
           break;
       }
 
       hasNoErrors = errorOperand == SUCCESSFUL_EXIT;
       hasCharsToProcess = nthInfixChar < strlen(infixString);
       isConvertingInfix = hasNoErrors && hasCharsToProcess;
+    }
+
+    // pop all current operators in stack and push them to queue
+    while (!isStackEmpty(OperatorStack)) {
+      enqueue(PostfixQueue, pop(OperatorStack));
     }
 
     return 0;
